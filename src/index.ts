f836ef170074567c5a5babed1039a94bb2cc165b
Higor Romano <higorjromano@gmail.com>
2026-02-27 18:28:57 -0300

    aula 4 pt1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+// type annotations
+
+const myName: string = "Higor"
+const myAge: number = 30
+const isStudent: boolean = true
+
+// observação: essa tipagem pode receber qualquer tipo de valor, o que não é recomendado.
+
+const all: any = "qualquer coisa"
+
+// observação 2
