789c85e042645553920ff08068bfed53b86ee8db
Higor Romano <higorjromano@gmail.com>
2026-03-09 09:22:50 -0300

    aula 8 pt1

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// Enum
+
+//enum Direction {
+   // North=1,
+   // South,
+   // East,
+   // West
+//}
+
+//const pos = {
+    //x: 100,
+   // y: 230,
+   // direction: Direction.East
+//}
+
+enum ClothingSize {
+    Small = 'P',
+    Medium = 'M',
+    Large = 'G',
+    ExtraLarge = 'GG'
+}
+
+const cloth = {
+    color: 'red',
+    size: ClothingSize.Medium
+}
