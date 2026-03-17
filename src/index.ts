b4185f724cfd259f72fb3fcc62a5116a72d671a7
Higor Romano <higorjromano@gmail.com>
2026-03-17 09:38:07 -0300

    aula10 pt4

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,24 @@ enum TrafficLightType {
 }
 
 interface YellowColor {
+    type: TrafficLightType.Yellow;
     wait(): void;
 }
 
 interface RedColor {
+    type: TrafficLightType.Red;
     stop(): void;
 }
 
 interface GreenColor {
+    type: TrafficLightType.Green;
     drive(): void;
 }
 
 type TrafficLight = YellowColor | RedColor | GreenColor;
 
 const trafficLight: TrafficLight = {
+    type: TrafficLightType.Yellow,
     wait() {
 
     },
