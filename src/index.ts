9c1e7923b8e8bd7b68fcd26d6abe85fdde95de78
Higor Romano <higorjromano@gmail.com>
2026-03-23 18:19:57 -0300

    aula 11 pt2

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ type Args = string | number | boolean;
 function bootstrap(dirname: string, args?: Args[]): boolean {
     return true;
 }
+
+type Mainfunction = (args: string[]) => void;
+
+const main: Mainfunction = (args) => {}
