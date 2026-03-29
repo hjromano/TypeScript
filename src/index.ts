dc117b6f2735928be6bfec8a550132aed33ee614
Higor Romano <higorjromano@gmail.com>
2026-03-29 14:37:45 -0300

    aula 12 pt2

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 export class Player {
     name: string;
-    health: number | undefined;
+    private health: number;
     constructor(name: string){
         this.name = name;
         this.health = 100;
     }
+    
+    public getHealth(){
+        return this.health;
+    }
+    public damage(amount: number){
+        const updatedHealth = this.health - amount;
+
+        if(updatedHealth <= 0){
+            this.health = 0;
+            this.die();
+            return
+        }
+        this.health = updatedHealth;
+    }
+    private die(){
+        console.log(this.name + " morreu!");
+    }
 
 }
 
-const player1 = new Player("Higor");
 
-player1.health;
+const player1 = new Player("Higor");
+player1.damage(30);
