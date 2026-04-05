94395c36d48773fafcac39b678191d32ea17f707
Higor Romano <higorjromano@gmail.com>
2026-04-05 06:11:20 -0300

    aula 12 pt7

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-interface Drivable{
-    startEngine(): void;
-    drive(): void;
+abstract class Shape {
+    abstract area(): number;
+
+    describe(): void {
+        console.log("Isso é uma forma geométrica.");
+    }
 }
 
-class Car implements Drivable{
-    startEngine(): void {
-        console.log("Engine started.");
+class Circle extends Shape {
+    radius: number;
+    constructor(radius: number) {
+        super();
+        this.radius = radius;
     }
-    drive(): void {
-        console.log("Car is driving.");
+    area(): number {
+        return Math.PI * this.radius ** 2;
+    }
+    describe(): void {
+        console.log("Isso é um círculo.");
     }
 }
+
+const circle = new Circle(5);
+circle.area(); // 78.53981633974483
+circle.describe(); // Isso é um círculo.
