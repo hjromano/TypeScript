f277fac5d01ace46ac663c4a756198a2cd82ca36
Higor Romano <higorjromano@gmail.com>
2026-04-08 11:20:53 -0300

    aula13 pt2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-new Date()
+/**
+ * Criar uma nova data a partir de outra
+ */
+function createDate(value: Date): Date;
+/**
+ * Criar uma nova data usando uma data por extenso
+ */
+function createDate(value: string): Date;
+/**
+ * Criar uma nova data usando uma data numerica
+ */
+function createDate(value: number): Date;
+function createDate(value: Date | string | number): Date {
+    return new Date(value);
+}
+
+createDate()
