caf07e0585502941ae6dc50dc78e91a74629b988
Higor Romano <higorjromano@gmail.com>
2026-04-14 09:29:31 -0300

    aula 14 pt1

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,3 @@
-interface Triangle {
-    sideA: number;
-    sideB: number;
-    sideC: number;
+function printValue(value: string | number) {
+    
 }
-interface Equilateral extends Triangle {
-    type: "equilateral";
-}
-interface Isosceles extends Triangle {
-    type: "isosceles";
-}
-interface Scalene extends Triangle {
-    type: "scalene";
-}
-type Triangles = Equilateral | Isosceles | Scalene
-
-class TriangleBuilder {
-    static build(sides: number): Equilateral;
-    static build(sideA: number, sideBC: number): Isosceles;
-    static build(sideA: number, sideB: number, sideC: number): Scalene;
-    static build(A: number, B?: number, C?: number){
-        if(B && C){
-            return {
-                type: "scalene",
-                sideA: A, sideB: B, sideC: C
-            }
-        }
-        return {
-            sideA: A, sideB: A, sideC: A,
-            type: "equilateral"
-        }
-     
-    }
-}
-
-TriangleBuilder.build()
