df41d63c78305918d565139dfc4486155fb06f91
Higor Romano <higorjromano@gmail.com>
2026-04-16 11:28:45 -0300

    aula 14 pt3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-function printValue(value: string | number) {
-    if (typeof value === "string") {
-        console.log(value.toUpperCase())
-    } else {
-        console.log(value.toFixed(2))
+class Dog {
+    bark() {
+        console.log("Woof!");
     }
-    
+}
+class Cat {
+    meow() {
+        console.log("Meow!");
+    }
+}
+
+
+function makeSound(animal: Dog | Cat) {
+    if (animal instanceof Dog) {
+        animal.bark();
+        return;
+    }
+    animal.meow();
 }
