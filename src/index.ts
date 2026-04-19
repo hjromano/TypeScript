46204eaa4e30c262507764ea13693bf72f2c4966
Higor Romano <higorjromano@gmail.com>
2026-04-19 05:53:50 -0300

    aula 14 pt6

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
-function main(value: string[] | Date){
-    if("push" in value){
-        value.push("Hello");
-    } else {
-        console.log(value.getDate());
+interface Chef {
+    cook(): void;
+    certificate: string;
+}
+
+interface Teacher {
+    teach(): void;
+    diploma: string;
+}
+
+interface Driver {
+    drive(): void;
+    license: string;
+}
+
+type Professionals = Chef | Teacher | Driver;
+
+function isChef(value: Professionals): value is Chef {
+    return (value as Chef).cook !== undefined;
+}
+
+function isTeacher(value: Professionals): value is Teacher {
+    return (value as Teacher).teach !== undefined;
+}
+
+function isDriver(value: Professionals): value is Driver {
+    return (value as Driver).drive !== undefined;
+}
+
+function execute(professional: Professionals) {
+    if (isChef(professional)) {
+        professional
+        return;
     }
+    if (isDriver(professional)) {
+        professional.drive();
+        return;
+    }
+    professional.teach();
+    
 }
