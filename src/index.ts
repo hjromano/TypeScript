0be6229ae7001d0fce940cd3b71841801d0ec95d
Higor Romano <higorjromano@gmail.com>
2026-04-23 08:48:42 -0300

    aula15 pt1

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,11 @@
-class Professionals {
-    isChef(): this is Chef {
-        return this instanceof Chef
+function printValue(value: string | number | boolean) {
+    if (typeof value === "string") {
+        value
+        return;
     }
-    isTeacher(): this is Teacher {
-        return this instanceof Teacher
+    if (typeof value === "number") {
+        value
+        return;
     }
-    isDriver(): this is Driver {
-        return this instanceof Driver
-    }
-
-
-}
-
-class Chef extends Professionals {
-    public cook(): void {}
-    public certificate: string = "Chef Higor"
-}
-class Teacher extends Professionals {
-    public teach(): void {}
-    public diploma: string = "Teacher Higor"
-}
-class Driver extends Professionals {
-    public drive(): void {}
-    public license: string = "Driver Higor"
-}
-
-function execute(professional: Professionals): void {
-    if (professional.isDriver()) {
-        professional.drive();
-    }
-
-    switch (true) {
-        case professional.isChef():{
-            professional.cook();
-            break;
-        }
-        case professional.isTeacher():{
-            professional.teach();
-            break;
-        }
-        case professional.isDriver():{
-            professional.drive();
-            break;
-        }
-    }
-
+    value;
 }
