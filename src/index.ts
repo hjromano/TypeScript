9a9f88cccdfc2fe0050337507f9d2d54b995f0e2
Higor Romano <higorjromano@gmail.com>
2026-04-25 07:53:14 -0300

    aula15 pt3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-function printValue(value: string | number | boolean) {
-    if (typeof value === "string" || typeof value === "number") {
-        value
+class Dog {
+    bark() {
+        console.log("Woof!");
+    }
+}
+
+class Cat {
+    meow() {
+        console.log("Meow!");
+    }
+}
+
+function makeSound(animal: Dog | Cat) {
+    if (animal instanceof Dog) {
+        animal.bark()
         return;
     }
+    animal.meow();
 
 }
+
+const dog = new Dog();
+makeSound(dog);
