97e5cd350dcba08e0d8380589300fddfed97afaa
Higor Romano <higorjromano@gmail.com>
2026-05-06 08:02:09 -0300

    aula16 pt5

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,19 @@ const removed = myCustomStringArr.remove();
 
 const myCustomNumberArr = new MyCustomArray<number>();
 myCustomNumberArr.add(42);
+
+interface Player {
+    name: string;
+    health: number;
+    shield: number;
+    isDead: boolean;
+}
+
+const players = new MyCustomArray<Player>();
+
+players.add({ name: "Player1", health: 100, shield: 50, isDead: false });
+
+const removedPlayer = players.remove();
+if (removedPlayer) {
+    removedPlayer.name
+}
