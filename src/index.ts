1ed347bff1a8219ddffdb536f69c4cbd2db4ce03
Higor Romano <higorjromano@gmail.com>
2026-05-07 08:35:24 -0300

    aula16 pt6

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,11 @@
-class MyCustomArray<T> {
-    private items: T[] = [];
-
-    public add(item: T){
-        this.items.unshift(item);
-    }
-    public remove(){
-        return this.items.shift()
-    }
-    public isEmpty(): boolean {
-        return this.items.length === 0;
-    }
+function loglength<T extends { length: number }>(item: T){
+    console.log(item.length);
 }
 
-const myCustomStringArr = new MyCustomArray<string>();
-myCustomStringArr.add("Hello");
-const removed = myCustomStringArr.remove();
+const numbers = [1, 2, 3, 4, 5];
+const myobj = { length: 10};
+const myName = "John Doe";
 
-const myCustomNumberArr = new MyCustomArray<number>();
-myCustomNumberArr.add(42);
-
-interface Player {
-    name: string;
-    health: number;
-    shield: number;
-    isDead: boolean;
-}
-
-const players = new MyCustomArray<Player>();
-
-players.add({ name: "Player1", health: 100, shield: 50, isDead: false });
-
-const removedPlayer = players.remove();
-if (removedPlayer) {
-    removedPlayer.name
-}
+loglength(numbers);
+loglength(myobj);
+loglength(myName);
